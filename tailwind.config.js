07eced60c0ee6fba3447c87f748d73b0d42b8e55
김영수 주임 연구원 <jklasd2001@musma.net>
2022-06-13 00:34:53 +0900

    feat: luxon 라이브러리 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     'src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: [
+          'Nunito Sans', 'sans-serif',
+        ]
+      }
+    },
   },
   plugins: [],
 }
